9c18e780fb4bbd7d7f5006958d6521b5ca5da8d7
ahwxl <javawxl@126.com>
2018-11-04 18:32:42 +0800

    init

--- src/main/java/com/shfft/cnap/anontation/block.java
+++ src/main/java/com/shfft/cnap/anontation/block.java
@@ -3,22 +3,32 @@
  */
 package com.shfft.cnap.anontation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import com.shfft.cnap.enums.Type;
 
 /**
- * @desc 
+ * @desc
  * @author wangxiaolei
  * @date 2018年11月4日 上午11:58:07
  */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface block {
 	
-	public int index = 0;
-	
-	public int lenght = 0;
-	
-	public Type type = Type.STRING;
-	
-	public String offset="L";
+	String value() default "";
 
-	
+	int index() default 0;
+
+	int lenght() default 0;
+
+	Type type() default Type.STRING;
+
+	String offset() default "L";
+
 }
--- src/main/java/com/shfft/cnap/service/impl/CityServiceImpl.java
+++ src/main/java/com/shfft/cnap/service/impl/CityServiceImpl.java
@@ -2,8 +2,11 @@ package com.shfft.cnap.service.impl;
 
 import com.shfft.cnap.model.domain.City;
 import com.shfft.cnap.service.CityService;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service("cityService")
+@Transactional
 public class CityServiceImpl extends BaseServiceImpl<City, Integer> implements CityService {
 }
